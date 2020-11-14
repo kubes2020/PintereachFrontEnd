a67b6d190be00b4884c8707a1d39b1195f3869fd
DavidHall-Code <david_m_hall@outlook.com>
2020-11-14 16:15:48 -0500

    Added Home.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+<header>
+  <h1>Welcomer</h1>
+  </header>
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
