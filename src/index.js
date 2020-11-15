62c8056259d1fbb630f3529bad3b95cba158f3b6
DavidHall-Code <david_m_hall@outlook.com>
2020-11-15 17:21:24 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
-import  './components/Login'
+import  Form from './components/Login'
 import './App.css'
 
 ReactDOM.render(
   
   <Router>
     <App />  
+    <Form />
   </Router>,
   document.getElementById('root')
 );
