32b7a25d14a330907920dc74b49c75be12e58c9f
David Hall <66974560+DavidHall-Code@users.noreply.github.com>
2020-11-15 23:06:10 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login'
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <Login />
     </div>
   );
 }
