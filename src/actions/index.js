8e6d1a014bb24e0c4e2790f11ca22ffd0521a679
rhea-manuel <rheamanuel02@gmail.com>
2020-11-16 13:41:33 -0500

    setup reducers & actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,24 @@
+import { axiosWithAuth } from "../utils/axiosWithAuth"
+
 export const LOGIN = 'login_action'
 export const REGISTER = 'register_action'
 export const ADD_ARTICLE = 'add_article_action'
+
+export const FETCH_DATA = 'fetch_data'
+export const FETCH_DATA_SUCCESS = 'fetch_data_success'
+export const FETCH_DATA_FAIL = 'fetch_data_fail'
+
+// Fetch Data, after login is finished, dispatch this function
+export const getData = () => (dispatch) => {
+    dispatch({ type: FETCH_DATA })
+
+    // REPLACE SERVERNAME WITH API CALL, depending on the data results, use id in object number
+    axiosWithAuth().get('SERVERNAME')
+        .then(res => {
+            // console.log(res)
+
+            // REPLACE DATA HERE, CHECK RES AND REPLACE
+            dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data })
+        })
+        .catch(err => console.log(err))
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,5 +5,25 @@ const initialState = {
 }
 
 export const rootReducer = (state = initialState, action) => {
-    
+    // console.log(action)
+    switch (action.type) {
+        case FETCH_DATA:
+            return {
+                ...state,
+                error: '',
+                articles: true
+            }
+
+        case FETCH_DATA_SUCCESS:
+            return {
+                ...state,
+                error: '',
+                isFetching: false,
+                articles: action.payload
+            }
+
+        default:
+            console.log("default")
+            return state
+    }
 }
