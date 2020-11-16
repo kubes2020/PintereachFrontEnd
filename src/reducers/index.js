f9589edfbf7cc8c7bac683b39fd348489e443bc0
rhea-manuel <rheamanuel02@gmail.com>
2020-11-16 16:05:47 -0500

    login is working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,16 +3,17 @@ import { FETCH_DATA, FETCH_DATA_SUCCESS, LOGIN, REGISTER } from '../actions'
 const initialState = {
     articles: [],
     error: '',
+    userId: ''
 }
 
 export const rootReducer = (state = initialState, action) => {
     // console.log(action)
     switch (action.type) {
-
         case LOGIN:
-            console.log('loggedIn')
+            // console.log(action.payload.id)
             return {
-                ...state
+                ...state,
+                userId: action.payload.user.id
             }
         
         case REGISTER:
