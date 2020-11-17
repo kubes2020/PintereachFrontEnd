866e1d73611cac4f6ca9f2d573b37e9e93c035dd
rhea-manuel <rheamanuel02@gmail.com>
2020-11-17 11:38:44 -0500

    protected route is working

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
 
-import Marketing from './components/Marketing';
-import Register from './components/Register.js'
-import Login from './components/Login'
-
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
 
 import { rootReducer } from './reducers'
+
+import Marketing from './components/Marketing';
+import Register from './components/Register.js'
+import Login from './components/Login'
+import Home from './components/Home';
+import PrivateRoute from './components/PrivateRoute'
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
@@ -23,6 +25,7 @@ function App() {
       <Route path="/register" render={(props) => {
         return <Register {...props} />
       }} />
+      <PrivateRoute exact path="/protected" component={Home} />
       <Route exact path="/">
         <Marketing></Marketing>
       </Route>
