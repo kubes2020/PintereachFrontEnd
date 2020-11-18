b6237b88ee6a046b599ce81b3e192895fc899645
Kylevb405 <kylevanburen405@gmail.com>
2020-11-18 17:18:21 -0500

    fix articlecard  bug?

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -21,7 +21,7 @@ const ArticleCard = props => {
                         <p className='card-text'>Rating: <span className='props'>{props.rating}</span></p>
                         <p className='card-text'>Category: <span className='props'>{props.category}</span></p>
                         <p className='card-text'>URL: <span className='props'>{props.art_url}</span></p>
-                        <a href={props.art_url} className='blue-button btn btn-primary'>Goto Article</a>
+                        <a href={props.art_url} className='blue-button btn btn-primary' rel = 'noreferrer' target='_blank' >Goto Article</a>
                     </div>
                 </div>
             </div>
