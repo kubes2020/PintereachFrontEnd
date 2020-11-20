a8cfb94aecc3fed0bd155842b3877a4604041a20
rhea-manuel <rheamanuel02@gmail.com>
2020-11-20 12:20:41 -0500

    go to url

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -20,7 +20,7 @@ const ArticleCard = props => {
                         <h1 className='card-title'><span className='props'>{props.art_name}</span></h1>
                         <p className='card-text'>Rating: <span className='props'>{props.rating}</span></p>
                         <p className='card-text'>Category: <span className='props'>{props.category}</span></p>
-                        <p className='card-text'>URL: <span className='props'>{props.art_url}</span></p>
+                        {/* <p className='card-text'>URL: <span className='props'>{props.art_url}</span></p> */}
                         <a href={props.art_url} className='blue-button btn btn-primary' rel = 'noreferrer' target='_blank' >Goto Article</a>
                     </div>
                 </div>
