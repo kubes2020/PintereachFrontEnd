b46c23e94b9fa041e2fa97caa4f96da087f1c044
Brian Kubes <briankubes@gmail.com>
2021-04-30 13:09:55 -0700

    added console.log to search submit in ArticleList to find concatenation issue with multiple searches

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,89 +1,100 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import ArticleCard from './ArticleCard';
-import '../style/filter.css'
-import '../style/login.css'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ArticleCard from "./ArticleCard";
+import "../style/filter.css";
+import "../style/login.css";
 const { connect } = require("react-redux");
 const { fetchData, filterArticles } = require("../actions");
 
 function ArticleList(props) {
+  const fetch = () => {
+    props.fetchData();
+  };
 
-    const fetch = () => {
-        props.fetchData()
-    }
+  useEffect(() => {
+    fetch();
+    // console.log
+  }, []);
 
-    useEffect(() => {
-        fetch()
-        // console.log
-    }, [])
+  const [search, setSearch] = useState();
 
-    const [search, setSearch] = useState()
+  const handleChanges = (e) => {
+    setSearch(e.target.value);
+  };
 
-    const handleChanges = (e) => {
-        setSearch(e.target.value)
-    }
+  const submit = (e) => {
+    console.log("this is search state:", search);
+    e.preventDefault();
+    props.filterArticles(search);
+  };
 
-    const submit = e => {
-        e.preventDefault()
-        props.filterArticles(search)
-    }
+  const reset = (e) => {
+    e.preventDefault();
+    props.filterArticles("");
+  };
 
-    const reset = e => {
-        e.preventDefault()
-        props.filterArticles('')
-    }
-
-    return (
-        <>
-            <form onSubmit={submit}>
-                <div className="form-group">
-                    <label for="category">Category:  </label>
-                    <select class="form-control" id="category" onChange={handleChanges} name='category'>
-                        <option value=''>---Select a Category---</option>
-                        <option value='Automotive'>Automotive</option>
-                        <option value='Books'>Books</option>
-                        <option value='Economics'>Economics</option>
-                        <option value='Education'>Education</option>
-                        <option value='Gaming'>Gaming</option>
-                        <option value='Humor'>Humor</option>
-                        <option value='Hobbies'>Hobbies</option>
-                        <option value='Movies'>Movies</option>
-                        <option value='Music'>Music</option>
-                        <option value='News'>News</option>
-                        <option value='Politics'>Politics</option>
-                        <option value='Recipes'>Recipes</option>
-                        <option value='Restaurants'>Restaurants</option>
-                        <option value='Sports'>Sports</option>
-                        <option value='Technology'>Technology</option>
-                        <option value='TV Shows'>TV Shows</option>
-                        <option value='Other'>Other</option>
-                    </select>
-                </div>
-                <Link onClick={reset} className="btn btn-danger"> Reset </Link>
-                <button className="blue-button btn btn-primary">Search</button>
-            </form>
-            <div className='test'>
-                {props.articles.map((item) => {
-                    return (
-                        <ArticleCard
-                            id={item.id}
-                            key={item.id}
-                            art_name={item.art_name}
-                            art_url={item.art_url}
-                            rating={item.rating}
-                            category={item.category}
-                        />)
-                })}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <form onSubmit={submit}>
+        <div className="form-group">
+          <label for="category">Category: </label>
+          <select
+            class="form-control"
+            id="category"
+            onChange={handleChanges}
+            name="category"
+          >
+            <option value="">---Select a Category---</option>
+            <option value="Automotive">Automotive</option>
+            <option value="Books">Books</option>
+            <option value="Economics">Economics</option>
+            <option value="Education">Education</option>
+            <option value="Gaming">Gaming</option>
+            <option value="Humor">Humor</option>
+            <option value="Hobbies">Hobbies</option>
+            <option value="Movies">Movies</option>
+            <option value="Music">Music</option>
+            <option value="News">News</option>
+            <option value="Politics">Politics</option>
+            <option value="Recipes">Recipes</option>
+            <option value="Restaurants">Restaurants</option>
+            <option value="Sports">Sports</option>
+            <option value="Technology">Technology</option>
+            <option value="TV Shows">TV Shows</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <Link onClick={reset} className="btn btn-danger">
+          {" "}
+          Reset{" "}
+        </Link>
+        <button className="blue-button btn btn-primary">Search</button>
+      </form>
+      <div className="test">
+        {props.articles.map((item) => {
+          return (
+            <ArticleCard
+              id={item.id}
+              key={item.id}
+              art_name={item.art_name}
+              art_url={item.art_url}
+              rating={item.rating}
+              category={item.category}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-        articles: state.articles,
-        isLoading: state.isLoading,
-    }
-}
+  return {
+    articles: state.articles,
+    isLoading: state.isLoading,
+  };
+};
 
-export default connect(mapStateToProps, { fetchData, filterArticles })(ArticleList)
+export default connect(mapStateToProps, { fetchData, filterArticles })(
+  ArticleList
+);
